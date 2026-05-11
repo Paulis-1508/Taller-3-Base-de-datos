bf5fd786d3d48b00284fd1bf26c62a615a4c5f7f
Paulis-1508 <mp.angulo@uniandes.edu.co>
2026-05-11 17:05:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#os.environ para despliegue. Descomente cuando ya probó todo local.
+#client = MongoClient(os.environ["MONGO_URI"])
+#client = MongoClient(os.environ["mongodb://ISIS2304E02202610:X8zbL7mua5ZQ@157.253.236.88:8087"])
+# TODO: conectarse al cluster Admonsis  
+# client = MongoClient("mongodb://<usuario>:<contraseña>@157.253.236.88:8087")
+client = MongoClient("mongodb://ISIS2304E02202610:X8zbL7mua5ZQ@157.253.236.88:8087")
+# TODO: conectarse a la base de datos Admonsis  
+# db = client["ISIS*******"]
+db = client["ISIS2304E02202610"]
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = list(db["comentarios"].find({"bar_id": bar_id},{"_id": 0}))
+    return comentarios
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    db["comentarios"].insert_one(datos)
+    return {'mensaje': 'Comentario guardado'}
+
+
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db["eventos"].find({"bar_id": bar_id},{"_id": 0}))
+    return eventos
+
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha_creacion']  = datetime.now().isoformat()
+    db["eventos"].insert_one(datos)
+    return {'mensaje': 'Evento guardado'}
+# Debe insertar el evento en la colección 'eventos'
+# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
